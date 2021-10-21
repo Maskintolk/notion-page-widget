c70dd37bf972714ba7f7789a37d29a539f3c0fba
Jakob Skov <jakob.skov@gmail.com>
2021-10-21 18:36:28 +0200

    Change build from library to web site

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
+const { resolve } = require("path");
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    lib: {
-      entry: "./index.html",
-      formats: ["es"],
-    },
     rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+      },
       external: /^lit-element/,
     },
   },
