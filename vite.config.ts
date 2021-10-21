3ac2e96822e00b475611310b3240aee42ba1dba7
Jakob Skov <jakob.skov@gmail.com>
2021-10-21 18:41:41 +0200

    Build fix for lit-element

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,13 @@
-const { resolve } = require("path");
+// https://vitejs.dev/config/
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
       },
-      external: /^lit-element/,
     },
   },
 });
